e8d2c754d2b05a08915021fab55266acffd80c89
sinse100 <sinse100@naver.com>
2026-01-15 13:34:50 +0900

    Update module1.c

--- os/rt/src/module1.c
+++ os/rt/src/module1.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+#include "module1.h"
+#include "module2.h"
+
+void module1_run(void)
+{
+    module2_print("called from module1_run()");
+}
+
+int module1_compute(int x)
+{
+    int result = module2_add(x, 10);
+    printf("module1: result = %d\n", result);
+    return result;
+}
